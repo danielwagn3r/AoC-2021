e77018e73fda1ac51df4ac3cf0a494f86cabcbf6
Daniel Wagner <daniel@wagners.name>
2021-12-02 16:12:26 +0100

    Add project files.

---
+++ AoC2021.Common/AoCClient.cs
@@ -0,0 +1,104 @@
+﻿namespace AoC2021.Common
+{
+    public class AoCClient
+    {
+        private readonly HttpClient _httpClient = new();
+        private readonly string _sessionToken;
+
+        public AoCClient(string sessionToken)
+        {
+            _sessionToken = sessionToken;
+        }
+
+        public async Task<string> GetInput(int day, int year)
+        {
+            var result = await GetCachedAsync(day, year);
+            if (result.IsNullOrEmpty())
+            {
+                result = await GetFromWebsiteAsync(day, year);
+                await PutCacheAsync(day, year, result);
+            }
+            return result ?? string.Empty;
+        }
+
+        private async Task<string> GetFromWebsiteAsync(int day, int year)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, $"https://adventofcode.com/{year}/day/{day}/input");
+            request.Headers.TryAddWithoutValidation("cookie", $"session={_sessionToken}");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task PutCacheAsync(int day, int year, string result)
+        {
+            var file = GetFileForDay(day, year);
+            await file.WriteAllTextAsync(result);
+        }
+
+        private async Task<string> GetCachedAsync(int day, int year)
+        {
+            var file = GetFileForDay(day, year);
+            return file.Exists ? await file.ReadAllTextAsync() : string.Empty;
+        }
+
+        private static FileInfo GetFileForDay(int day, int year)
+        {
+            var dir = Directory.CreateDirectory("Cache");
+            var file = dir.GetFileInfo($"{year}_{day}.txt");
+            return file;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static bool IsNullOrEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+
+        public static async Task<string> ReadAllTextAsync(this FileInfo fileInfo)
+        {
+            if (!fileInfo.Exists)
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                return await File.ReadAllTextAsync(fileInfo.FullName);
+            }
+            catch (Exception e)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static async Task<FileInfo> WriteAllTextAsync(this FileInfo fileInfo, string content)
+        {
+            await File.WriteAllTextAsync(fileInfo.FullName, content);
+            return fileInfo;
+        }
+
+        public static FileInfo GetFileInfo(this DirectoryInfo directoryInfo, string fileName)
+        {
+            return new FileInfo(Path.Combine(directoryInfo.FullName, fileName));
+        }
+
+        public static IEnumerable<string> GetLines(this string str, bool removeEmptyLines = false)
+        {
+            using (var sr = new StringReader(str))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (removeEmptyLines && string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    yield return line;
+                }
+            }
+        }
+    }
+}
---
+++ AoC2021.Day1.App/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AoC2021.Common;
+
+string sessionToken = "53616c7465645f5fa8e69a3592c87eaa604b1c639256aa8ca1e07c56f6f025c624065bea1ec0e4758e2717813b4c799e";
+
+var client = new AoCClient(sessionToken);
+var result = await client.GetInput(1, 2021);
+
+var input = result.GetLines(false).Select(line => int.Parse(line)).ToArray();
+
+Console.WriteLine($"One: {PuzzleOne(input)}");
+Console.WriteLine($"Two: {PuzzleTwo(input)}");
+
+int PuzzleOne(int[] input)
+{
+    int count = 0;
+
+    for (int i = 1; i < input.Length; i++)
+    {
+        if (input[i] > input[i - 1])
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int PuzzleTwo(int[] input)
+{
+    var windows = GetWindows(input);
+
+    return PuzzleOne(windows);
+}
+
+int[] GetWindows(int[] input)
+{
+    var r2 = input.Zip(input.Skip(1), (a, b) => Tuple.Create(a, b));
+    var result = r2.Zip(input.Skip(2), (a, b) => Tuple.Create(a.Item1, a.Item2, b));
+
+    var windows = result.Select(t => t.Item1 + t.Item2 + t.Item3);
+
+    return windows.ToArray();
+}
