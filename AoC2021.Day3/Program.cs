928f6f1f5d252222edb873f29d5d0c3ce23c2e5c
Daniel Wagner <daniel@wagners.name>
2021-12-03 12:13:31 +0100

    Added day 3, puzzle 2.

--- AoC2021.Day3/Program.cs
+++ AoC2021.Day3/Program.cs
@@ -4,23 +4,28 @@ Console.WriteLine("AoC 2021 Day 3");
 var input = (await File.ReadAllLinesAsync("input.txt")).ToArray();
 
 Console.WriteLine($"One: {PuzzleOne(input)}");
-//Console.WriteLine($"Two: {PuzzleTwo(input)}");
+Console.WriteLine($"Two: {PuzzleTwo(input)}");
 
 int PuzzleOne(string[] input)
 {
     int gamma;
     int epsilon;
 
-    string[] trans = new string[input[0].Length];
+    string[] trans = Transpose(input);
 
-    for (int i = 0; i < input[0].Length; i++)
-    {
-        trans[i] = new string(input.Select(x => x[i]).ToArray());
-    }
+    var gammaStr = new string(trans.Select(l =>
+        new
+        {
+            Zeros = l.Where(b => b == '0').Count(),
+            Ones = l.Where(b => b == '1').Count()
+        }).Select(t => t.Ones > t.Zeros ? '1' : '0').ToArray());
 
-
-    var gammaStr = new string(trans.Select(l => new { Zeros = l.Where(b => b == '0').Count(), Ones = l.Where(b => b == '1').Count() }).Select(t => t.Ones > t.Zeros ? '1' : '0').ToArray());
-    var epsilonStr = new string(trans.Select(l => new { Zeros = l.Where(b => b == '0').Count(), Ones = l.Where(b => b == '1').Count() }).Select(t => t.Ones < t.Zeros ? '1' : '0').ToArray());
+    var epsilonStr = new string(trans.Select(l =>
+        new
+        {
+            Zeros = l.Where(b => b == '0').Count(),
+            Ones = l.Where(b => b == '1').Count()
+        }).Select(t => t.Ones < t.Zeros ? '1' : '0').ToArray());
 
     gamma = Convert.ToInt32(gammaStr, 2);
     epsilon = Convert.ToInt32(epsilonStr, 2);
@@ -30,5 +35,48 @@ int PuzzleOne(string[] input)
 
 int PuzzleTwo(string[] input)
 {
-    throw new NotImplementedException();
+    int oxygen;
+    int co2Scrubber;
+
+    var search = input;
+
+    for (int i = 0; i < input[0].Length && search.Length > 1; i++)
+    {
+        string[] trans = Transpose(search);
+
+        var bitCounts = trans.Select(l => new { Zeros = l.Where(b => b == '0').Count(), Ones = l.Where(b => b == '1').Count() }).ToArray();
+        var mcValue = bitCounts.Select(c => c.Ones >= c.Zeros ? '1' : '0').ToArray();
+
+        search = search.Where(b => b[i] == mcValue[i]).ToArray();
+    }
+
+    oxygen = Convert.ToInt32(search[0], 2);
+
+    search = input;
+
+    for (int i = 0; i < input[0].Length && search.Length > 1; i++)
+    {
+        string[] trans = Transpose(search);
+
+        var bitCounts = trans.Select(l => new { Zeros = l.Where(b => b == '0').Count(), Ones = l.Where(b => b == '1').Count() }).ToArray();
+        var lcValue = bitCounts.Select(c => c.Ones < c.Zeros ? '1' : '0').ToArray();
+
+        search = search.Where(b => b[i] == lcValue[i]).ToArray();
+    }
+
+    co2Scrubber = Convert.ToInt32(search[0], 2);
+
+    return oxygen * co2Scrubber;
+}
+
+string[] Transpose(string[] input)
+{
+    string[] trans = new string[input[0].Length];
+
+    for (int i = 0; i < input[0].Length; i++)
+    {
+        trans[i] = new string(input.Select(x => x[i]).ToArray());
+    }
+
+    return trans;
 }
