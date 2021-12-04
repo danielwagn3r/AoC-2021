fa92337344bef44e87bac22b16d91d2ea9a2d278
Daniel Wagner <daniel@wagners.name>
2021-12-04 16:15:54 +0100

    Added day 4, puzzle 1

---
+++ AoC2021.Day4/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("AoC 2021 Day 4");
+
+var input = (await File.ReadAllLinesAsync("input2.txt")).Select(l => l.TrimStart()).ToArray();
+
+var boardDim = Regex.Split(input[2], " +").Length;
+
+var boardNum = input.Skip(1).Count() / (boardDim + 1);
+
+var draw = input[0].Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+
+var boards = new List<IEnumerable<Element>>();
+
+for (int i = 0; i < boardNum; i++)
+{
+    var elements = input.Skip(i * (boardDim + 1) + 2)
+        .Take(boardDim)
+        .Select((l, r) =>
+            Regex.Split(l, " +").Select((n, c) => new Element { Value = Convert.ToInt32(n), X = r, Y = c, Marked = false })).SelectMany(x => x).ToArray();
+
+    boards.Add(elements);
+}
+
+Console.WriteLine($"One: {PuzzleOne(draw, boards)}");
+Console.WriteLine($"Two: {PuzzleTwo(draw, boards)}");
+
+int PuzzleOne(int[] draw, IList<IEnumerable<Element>> boards)
+{
+    var rows = new int[boardNum, boardDim];
+    var cols = new int[boardNum, boardDim];
+    bool finished = false;
+    int lastDraw = 0;
+    int lastBoard = 0;
+
+    foreach (int d in draw)
+    {
+        for (int bi = 0; bi < boardNum; bi++)
+        {
+            boards[bi].Select((e, i) => new { e, i })
+                .Where(ie => ie.e.Value == d)
+                .AsParallel().ForAll(x =>
+            {
+                x.e.Marked = true;
+                if (++rows[bi, x.e.Y] >= boardDim) { finished = true; lastBoard = bi; }
+                if (++cols[bi, x.e.X] >= boardDim) { finished = true; lastBoard = bi; }
+            });
+        }
+
+        if (finished)
+        {
+            lastDraw = d;
+            break;
+        }
+    }
+
+    return boards[lastBoard].Where(e => !e.Marked).Select(e => e.Value).Sum() * lastDraw;
+}
+
+int PuzzleTwo(int[] draw, IList<IEnumerable<Element>> boards)
+{
+    return 0;
+}
+
+internal class Element
+{
+    public int Value { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool Marked { get; set; }
+}
