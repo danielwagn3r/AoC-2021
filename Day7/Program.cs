52c28c6c9716958cb9a0871a90bd90a966594dd5
Daniel Wagner <daniel@wagners.name>
2021-12-07 09:13:21 +0100

    Added day 7, puzzle 1

---
+++ Day7/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AoC 2021 Day 7");
+
+var input = (await File.ReadAllLinesAsync("input.txt")).Select(line => int.Parse(line)).ToArray();
+
+Console.WriteLine($"One: {PuzzleOne(input)}");
+Console.WriteLine($"Two: {PuzzleTwo(input)}");
+
+int PuzzleOne(int[] input)
+{
+	var positions = input.Distinct();
+
+	var fuel = new Dictionary<int, int>();
+
+	foreach (var pos in positions)
+	{
+		fuel.Add(pos, input.Select(i => Math.Abs(i - pos)).Sum());
+	}
+
+	return fuel.OrderBy(f => f.Value).First().Value;
+}
+
+int PuzzleTwo(int[] input)
+{
+	return 0;
+}
