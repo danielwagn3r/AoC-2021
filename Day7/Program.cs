da3c32e95dbc6be5d14735a4b2389c5a1d7e82b2
Daniel Wagner <daniel@wagners.name>
2021-12-08 13:58:51 +0100

    Fixed source formatting.

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -8,28 +8,28 @@ Console.WriteLine($"Two: {PuzzleTwo(input)}");
 
 int PuzzleOne(int[] input)
 {
-	var positions = input.Distinct();
+    var positions = input.Distinct();
 
-	var fuel = new Dictionary<int, int>();
+    var fuel = new Dictionary<int, int>();
 
-	foreach (var pos in positions)
-	{
-		fuel.Add(pos, input.Select(i => Math.Abs(i - pos)).Sum());
-	}
+    foreach (var pos in positions)
+    {
+        fuel.Add(pos, input.Select(i => Math.Abs(i - pos)).Sum());
+    }
 
-	return fuel.OrderBy(f => f.Value).FirstOrDefault().Value;
+    return fuel.OrderBy(f => f.Value).FirstOrDefault().Value;
 }
 
 int PuzzleTwo(int[] input)
 {
-	var positions = Enumerable.Range(input.Min(), input.Max());
+    var positions = Enumerable.Range(input.Min(), input.Max());
 
-	var fuel = new Dictionary<int, int>();
+    var fuel = new Dictionary<int, int>();
 
-	foreach (var pos in positions)
-	{
-		fuel.Add(pos, input.Select(i => Enumerable.Range(0, Math.Abs(i - pos) + 1).Sum()).Sum());
-	}
+    foreach (var pos in positions)
+    {
+        fuel.Add(pos, input.Select(i => Enumerable.Range(0, Math.Abs(i - pos) + 1).Sum()).Sum());
+    }
 
-	return fuel.OrderBy(f => f.Value).FirstOrDefault().Value;
+    return fuel.OrderBy(f => f.Value).FirstOrDefault().Value;
 }
