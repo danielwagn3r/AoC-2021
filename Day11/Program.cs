be1589658fd87818e39e5936935e050f06d7bf91
Daniel Wagner <daniel@wagners.name>
2021-12-11 20:32:56 +0100

    Added day 11, puzzle 2.

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -10,159 +10,208 @@ int[,] octos = new int[maxX, maxY];
 
 for (int x = 0; x < maxX; x++)
 {
-for (int y = 0; y < maxY; y++)
-{
-octos[x, y] = input[x][y];
-}
+    for (int y = 0; y < maxY; y++)
+    {
+        octos[x, y] = input[x][y];
+    }
 }
 
 Console.WriteLine($"One: {PuzzleOne(octos)}");
+
+for (int x = 0; x < maxX; x++)
+{
+    for (int y = 0; y < maxY; y++)
+    {
+        octos[x, y] = input[x][y];
+    }
+}
+
 Console.WriteLine($"Two: {PuzzleTwo(octos)}");
 
 int PuzzleOne(int[,] octos)
 {
-int flashes = 0;
+    int flashes = 0;
 
-for (int i = 0; i < iterations; i++)
-{
-bool[,] flashed = new bool[maxX, maxY];
-List<Point> flashing = new();
+    for (int i = 0; i < iterations; i++)
+    {
+        bool[,] flashed = new bool[maxX, maxY];
+        List<Point> flashing = new();
 
-for (int x = 0; x < maxX; x++)
-{
-for (int y = 0; y < maxY; y++)
-{
-if (++octos[x, y] > 9)
-{
-flashing.Add(new Point(x, y));
-flashes++;
-}
-}
-}
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                if (++octos[x, y] > 9)
+                {
+                    flashing.Add(new Point(x, y));
+                    flashes++;
+                }
+            }
+        }
 
-foreach (var octo in flashing)
-{
-flashes += FlashOcto(ref octos, ref flashed, octo);
-}
-}
-return flashes;
+        foreach (var octo in flashing)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, octo);
+        }
+    }
+
+    return flashes;
 }
 
 int FlashOcto(ref int[,] octos, ref bool[,] flashed, Point octo)
 {
-if (flashed[octo.X, octo.Y])
-return 0;
+    if (flashed[octo.X, octo.Y])
+        return 0;
 
-List<Point> flashing = new();
+    List<Point> flashing = new();
 
-if (octos[octo.X, octo.Y] <= 9)
-{
-if (++octos[octo.X, octo.Y] > 9)
-return 1 + FlashOcto(ref octos, ref flashed, octo);
+    if (octos[octo.X, octo.Y] <= 9)
+    {
+        if (++octos[octo.X, octo.Y] > 9)
+            return 1 + FlashOcto(ref octos, ref flashed, octo);
 
-return 0;
-}
-else
-{
-int flashes = 0;
-octos[octo.X, octo.Y] = 0;
-flashed[octo.X, octo.Y] = true;
+        return 0;
+    }
+    else
+    {
+        int flashes = 0;
+        octos[octo.X, octo.Y] = 0;
+        flashed[octo.X, octo.Y] = true;
 
-// Up-Left
-if (octo.X > 0 && octo.Y > 0)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X - 1, octo.Y - 1));
-}
+        // Up-Left
+        if (octo.X > 0 && octo.Y > 0)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X - 1, octo.Y - 1));
+        }
 
-// Up
-if (octo.X > 0)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X - 1, octo.Y));
-}
+        // Up
+        if (octo.X > 0)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X - 1, octo.Y));
+        }
 
-// Up-Right
-if (octo.X > 0 && octo.Y < maxY - 1)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X - 1, octo.Y + 1));
-}
+        // Up-Right
+        if (octo.X > 0 && octo.Y < maxY - 1)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X - 1, octo.Y + 1));
+        }
 
-// Down
-if (octo.X < maxX - 1)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X + 1, octo.Y));
-}
+        // Down
+        if (octo.X < maxX - 1)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X + 1, octo.Y));
+        }
 
-// Down-Left
-if (octo.X < maxX - 1 && octo.Y > 0)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X + 1, octo.Y - 1));
-}
+        // Down-Left
+        if (octo.X < maxX - 1 && octo.Y > 0)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X + 1, octo.Y - 1));
+        }
 
-// Down-Right
-if (octo.X < maxX - 1 && octo.Y < maxY - 1)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X + 1, octo.Y + 1));
-}
+        // Down-Right
+        if (octo.X < maxX - 1 && octo.Y < maxY - 1)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X + 1, octo.Y + 1));
+        }
 
-// Left
-if (octo.Y > 0)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X, octo.Y - 1));
-}
+        // Left
+        if (octo.Y > 0)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X, octo.Y - 1));
+        }
 
-// Right
-if (octo.Y < maxY - 1)
-{
-flashes += FlashOcto(ref octos, ref flashed, new(octo.X, octo.Y + 1));
-}
+        // Right
+        if (octo.Y < maxY - 1)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, new(octo.X, octo.Y + 1));
+        }
 
-return flashes;
-}
+        return flashes;
+    }
 }
 
 int PuzzleTwo(int[,] octos)
 {
-return 0;
+    int flashes = 0;
+    int i;
+
+    for (i = 0; ; i++)
+    {
+        bool[,] flashed = new bool[maxX, maxY];
+        List<Point> flashing = new();
+
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                if (++octos[x, y] > 9)
+                {
+                    flashing.Add(new Point(x, y));
+                    flashes++;
+                }
+            }
+        }
+
+        foreach (var octo in flashing)
+        {
+            flashes += FlashOcto(ref octos, ref flashed, octo);
+        }
+
+        int sumFlashed = 0;
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int y = 0; y < maxY; y++)
+            {
+                if (flashed[x, y])
+                    sumFlashed++;
+            }
+        }
+
+        if (sumFlashed == maxX * maxY)
+            break;
+    }
+
+    return i + 1;
 }
 
 (int, int) GetDimensions(int[][] input) => (input.Length, input[0].Length);
 
 internal class Point : IEquatable<Point>
 {
-	public int X { get; set; }
-	public int Y { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
 
-	public Point(int x, int y)
-	{
-		X = x;
-		Y = y;
-	}
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
 
-	public override bool Equals(Object obj)
-	{
-		if (obj == null)
-			return false;
+    public override bool Equals(Object obj)
+    {
+        if (obj == null)
+            return false;
 
-		if (obj is not Point point)
-			throw new ArgumentException("Object is not a Point.");
+        if (obj is not Point point)
+            throw new ArgumentException("Object is not a Point.");
 
-		return this.X == point.X && this.Y == point.Y;
-	}
+        return this.X == point.X && this.Y == point.Y;
+    }
 
-	public override int GetHashCode()
-	{
-		int hashX = X.GetHashCode();
+    public override int GetHashCode()
+    {
+        int hashX = X.GetHashCode();
 
-		int hashY = Y.GetHashCode();
+        int hashY = Y.GetHashCode();
 
-		return hashX ^ hashY;
-	}
+        return hashX ^ hashY;
+    }
 
-	public bool Equals(Point obj)
-	{
-		if (obj == null)
-			return false;
+    public bool Equals(Point obj)
+    {
+        if (obj == null)
+            return false;
 
-		return this.X == obj.X && this.Y == obj.Y;
-	}
+        return this.X == obj.X && this.Y == obj.Y;
+    }
 }
